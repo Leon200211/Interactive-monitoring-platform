a877c6d0425888994cb502b4e9b08805ad5e2dc1
Leon200211 <leon200207@yandex.ru>
2023-05-05 08:22:31 +0300

    Начал менять структуру

---
+++ website/public_html/include/asider.php
@@ -0,0 +1,41 @@
+<div class="app-sidebar colored">
+    <div class="sidebar-header">
+        <a class="header-brand" href="index.html">
+            <div class="logo-img">
+                <img src="assets/imgs/smlt_logo_white_ru.png" class="header-brand-img" alt="lavalite" width="125px">
+            </div>
+        </a>
+    </div>
+
+    <div class="sidebar-content">
+        <div class="nav-container">
+            <nav id="main-menu-navigation" class="navigation-main">
+                <div class="nav-lavel">Навигация</div>
+                <div class="nav-item active">
+                    <a href="index.html"><i class="ik ik-home"></i><span>Главная</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="zk-cards.html"><i class="ik ik-menu"></i><span>Проекты</span></a>
+                </div>
+
+
+                <div class="nav-lavel">Приложения</div>
+                <div class="nav-item">
+                    <a href="pages/calendar.html"><i class="ik ik-calendar"></i><span>Календарь</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="pages/taskboard.html"><i class="ik ik-server"></i><span>Панель задач</span></a>
+                </div>
+
+
+                <div class="nav-lavel">Поддержка</div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)"><i class="ik ik-monitor"></i><span>Документация</span></a>
+                </div>
+                <div class="nav-item">
+                    <a href="javascript:void(0)"><i class="ik ik-help-circle"></i><span>Отправить вопрос</span></a>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
--- website/public_html/login.php
+++ website/public_html/login.php
@@ -1,25 +1,7 @@
-<!doctype html>
-<html class="no-js" lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Login | ThemeKit - Admin Template</title>
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require_once "include/head.php";
+?>
 
-    <link rel="icon" href="assets/imgs/samolet_logo_sign_blue.png" type="image/x-icon" />
-
-    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
-
-    <link rel="stylesheet" href="assets/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="assets/node_modules/ionicons/dist/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/node_modules/icon-kit/dist/css/iconkit.min.css">
-    <link rel="stylesheet" href="assets/node_modules/perfect-scrollbar/css/perfect-scrollbar.css">
-    <link rel="stylesheet" href="dist/css/theme.min.css">
-    <script src="assets/src/js/vendor/modernizr-2.8.3.min.js"></script>
-</head>
 
 <body>
 
@@ -27,7 +9,7 @@
     <div class="container-fluid h-100">
         <div class="row flex-row h-100 bg-white">
             <div class="col-xl-8 col-lg-6 col-md-5 p-0 d-md-block d-lg-block d-sm-none d-none">
-                <div class="lavalite-bg" style="background-image: url('img/auth/login-bg.jpeg')">
+                <div class="lavalite-bg" style="background-image: url('assets/img/auth/login-bg.jpeg')">
                     <div class="lavalite-overlay"></div>
                 </div>
             </div>
@@ -66,21 +48,6 @@
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-<script>window.jQuery || document.write('<script src="src/js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
-<script src="assets/node_modules/popper.js/dist/umd/popper.min.js"></script>
-<script src="assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="assets/node_modules/perfect-scrollbar/dist/perfect-scrollbar.min.js"></script>
-<script src="assets/node_modules/screenfull/dist/screenfull.js"></script>
-<script src="assets/dist/js/theme.js"></script>
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-<script>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-        e.src='https://www.google-analytics.com/analytics.js';
-        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-    ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-</script>
-</body>
-</html>
+<?php
+require_once "include/footer.php";
+?>
